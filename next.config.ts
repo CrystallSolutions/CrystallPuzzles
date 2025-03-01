ed662b45bbf2cb16b7089369d82e6fb6b0c09965
Einsy <eins.yeager@gmail.com>
2025-03-01 22:30:51 +0400

    f

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/crystal_puzzles",
   sassOptions: {
     additionalData: `$color: red;`,
   },
